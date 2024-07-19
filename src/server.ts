d884ee85b548faf828f9a94192665e0e4683ff7a
samm002 <samuelsdjodis@gmail.com>
2024-07-19 22:24:04 +0700

    adding ip log in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import axios from "axios";
 
 import { PORT } from "@/utils/env";
 import routes from "@/routes";
@@ -15,9 +16,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/v1", routes)
 
-app.get("/", (req: express.Request, res: express.Response) => {
-  res.send('Welcome to Top Up Prototype!');
+app.get("/", async (req: express.Request, res: express.Response) => {
+  try {
+    const response = await axios.get('https://api.ipify.org?format=json');
+    res.send(`Welcome to Top Up Prototype!, server ip : ${response.data.ip}`);
+  } catch (error) {
+    res.status(500).send('Error retrieving IP address');
+  }
 });
+
+
 
 app.listen(PORT, () => {
   console.log(`Server is running at ${HOSTNAME || "http://localhost"}:${PORT}`);
